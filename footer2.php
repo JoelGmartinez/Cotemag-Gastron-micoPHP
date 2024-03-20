b52008b1c848c2ce02ab0dc95856776ffa591480
JoelGmartinez <joelstiven195@gmail.com>
2024-03-20 15:03:37 -0500

    links

--- footer2.php
+++ footer2.php
@@ -7,23 +7,23 @@
                             <h3>COTEMAG</h3>
                             <ul>
                                 <li><i class="bi bi-telephone-outbound"></i> (+57) 605-4231290 / 605-4230603</li>
-                                <li><a href="#"><i class="bi bi-whatsapp"></i> (+57) 3004813444</a></li>
+                                <li><a href="https://api.whatsapp.com/send?phone=573004813444&text=Hola%2C+estoy+interesado+en+matricularme+en+COTEMAG" target="_blank"><i class="bi bi-whatsapp"></i> (+57) 3004813444</a></li>
                                 <li><i class="bi bi-geo-alt-fill"></i> Avenida Los Estudiantes #17 - 46</li>
                                 <li><i class="fa-thin fa-earth-americas"></i> Santa Marta - Colombia</li>
-                                <li><a href="#"><i class="bi bi-envelope"></i> direcciongeneral@cotemag.edu.co</a></li>
+                                <li><a href="direcciongeneral@cotemag.edu.co" target="_blank"><i class="bi bi-envelope"></i> direcciongeneral@cotemag.edu.co</a></li>
                             </ul>
                         </div>
                         <div class="footer__soporte">
                             <h3>Ediciones</h3>
                             <ul>
                                 <li>
-                                    <a href="#">Educacion Superior</a>
+                                    <a href="#" target="_blank">Educacion Superior</a>
                                 </li>
                                 <li>
-                                    <a href="#">Educacion para el trabajo</a>
+                                    <a href="#" target="_blank">Educacion para el trabajo</a>
                                 </li>
                                 <li>
-                                    <a href="#">Colegios</a>
+                                    <a href="#" target="_blank">Colegios</a>
                                 </li>
                             </ul>
                         </div>
@@ -31,19 +31,19 @@
                             <h3>Soporte Tecnico</h3>
                             <ul>
                                 <li>
-                                    <a href="#">Contactenos</a>
+                                    <a href="https://api.whatsapp.com/send?phone=573004813444&text=Hola%2C+estoy+interesado+en+matricularme+en+COTEMAG" target="_blank">Contactenos</a>
                                 </li>
                                 <li>
-                                    <a href="#">Pagos en linea</a>
+                                    <a href="#" target="_blank">Pagos en linea</a>
                                 </li>
                                 <li>
-                                    <a href="#">Acceso remoto</a>
+                                    <a href="#" target="_blank">Acceso remoto</a>
                                 </li>
                                 <li>
-                                    <a href="#">Empresa</a>
+                                    <a href="#" target="_blank">Empresa</a>
                                 </li>
                                 <li>
-                                    <a href="#">Estado del servicio</a>
+                                    <a href="https://status.q10.com/" target="_blank">Estado del servicio</a>
                                 </li>
                             </ul>
                         </div>
@@ -53,7 +53,7 @@
             </div>
             <div class="i-redes">
                 <ul class="iconos">
-                    <li class="list-inline-item"><a href="#"><i class="bi bi-facebook"></i></i></a></li>
+                    <li class="list-inline-item"><a href="https://www.facebook.com/cotemagsm" target="_blank"><i class="bi bi-facebook"></i></i></a></li>
                     <li class="list-inline-item"><a href="https://www.instagram.com/cotemaggastronomico/" target="_blank"><i class="bi bi-instagram"></i></i></a></li>
                     <li class="list-inline-item"><a href="https://api.whatsapp.com/send/?phone=573004813444&text&type=phone_number&app_absent=0" target="_blank"><i class="bi bi-whatsapp"></i></a></li>
                 </ul>
